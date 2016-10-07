7317772e899b97fe974c332a5f90d6ded9a9cbca
Mark Servilla <mark.servilla@gmail.com>
2016-10-07 17:09:58 -0600

    Add new working files

---
+++ list_data_entities.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""":Mod: list_data_entities
+
+:Synopsis:
+
+:Author:
+    servilla
+  
+:Created:
+    10/7/16
+"""
+
+import logging
+
+logging.basicConfig(format='%(asctime)s %(levelname)s (%(name)s): %(message)s',
+                    datefmt='%Y-%m-%d% H:%M:%S%z')
+logging.getLogger('').setLevel(logging.WARN)
+logger = logging.getLogger('list_data_entities')
+
+
+def main():
+    return 0
+
+
+if __name__ == "__main__":
+    main()
