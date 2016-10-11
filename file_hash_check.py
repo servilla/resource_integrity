190d032427dab3447068fc7c957657fa5c88e851
Mark Servilla <mark.servilla@gmail.com>
2016-10-11 13:12:41 -0600

    Add SHA1 hash functionality.

--- file_hash_check.py
+++ file_hash_check.py
@@ -27,15 +27,25 @@ logger = logging.getLogger('file_hash_check')
 import getopt
 import sys
 from os.path import join, getsize
+from hashlib import sha1
 
 from list_data_entities import list_data_entities
 
 
+def sha1sum(file_name):
+    hash = sha1()
+    with open(file_name, "rb") as f:
+        for chunk in iter(lambda: f.read(128 * hash.block_size), b""):
+            hash.update(chunk)
+    return hash.hexdigest()
+
+
 def main(argv):
-    usage = 'Usage: python ./file_size_check -h | [-r report file] ' \
+    usage = 'Usage: python ./file_hash_check -h | [-r report file] ' \
             '<data_entity_file>'
-    synopsis = '"file_size_check" will compare data entity sizes with ' \
-               'file system file sizes and report any inconsistencies.'
+    synopsis = '"file_hash_check" will compare data entity SHA1 hash value ' \
+               'with one generated from the file system object and report ' \
+               'any inconsistencies.'
 
     if len(argv) == 0:
         logger.error(usage)
@@ -76,17 +86,17 @@ def main(argv):
         ename = eparts[3]
         efile = join(epath, epid, ename)
         try:
-            esize = int(eparts[5])
+            ehash = int(eparts[4])
         except ValueError as e:
             print('ValueError: {0} for {1}'.format(e, entity), file=report)
         try:
-            fsize = getsize(efile)
-            if not esize == fsize:
-                print('{0}: expected {1}, but found {2}'.format(efile, esize,
-                                                                fsize),
+            fhash = sha1sum(efile)
+            if not ehash == fhash:
+                print('{0}: expected {1}, but found {2}'.format(efile, ehash,
+                                                                fhash),
                       file=report)
-        except OSError as e:
-            print('OSError: {0}'.format(e), file=report)
+        except IOError as e:
+            print('IOError: {0}'.format(e), file=report)
 
     return 0
 
